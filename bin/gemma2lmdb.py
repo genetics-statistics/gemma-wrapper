57f3b12304b93bb1427e11b9a8c606045fa1d027
Pjotr Prins <pjotr.public01@thebird.nl>
2023-12-29 17:54:46 +0100

    Started on export to lmdb - using python because the Ruby lmdb version only does strings

---
+++ bin/gemma2lmdb.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import lmdb
+from struct import *
+
+parser = argparse.ArgumentParser(description='Turn GEMMA assoc output into an lmdb db.')
+parser.add_argument('--db',default="gemma",help="DB name")
+parser.add_argument('files',nargs='*',help="GEMMA assoc file(s)")
+args = parser.parse_args()
+
+with lmdb.open(args.db+".mdb",subdir=False) as env:
+    for fn in args.files:
+        print(f"Processing {fn}...")
+        with open(fn) as f:
+            for line in f.readlines():
+                chr,rs,pos,miss,a1,a0,af,logl_H1,l_mle,p_lrt = line.rstrip('\n').split('\t')
+                if chr=='chr':
+                    continue
+                print(chr,rs)
+                with env.begin(write=True) as txn:
+                    key = (chr+'_'+pos).encode()
+                    key = pack('=2sL',chr.encode(),int(pos))
+                    val = pack('=hhlf', 1, 2, 3, 1.0)
+                    assert len(val)==12, f"Packed size is expected to be 12, but is {len(val)}"
+                    res = txn.put(key, bytes(val), dupdata=False, overwrite=False)
+                    assert res,f"Failed to update lmdb record with key {key} -- probably a duplicate"
