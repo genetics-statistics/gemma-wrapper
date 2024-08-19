ddca57e178eedddf274ecc09486efc311216526b
Pjotr Prins <pjotr.public01@thebird.nl>
2024-08-19 10:23:14 +0200

    Convert GN geno files to BIMBAM

---
+++ bin/gn-geno-to-gemma.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+#
+# Takes a GN geno file and writes two files. One JSON file with metadata and a BIMBAM file to stdout.
+#
+# Note the simple JSON file contains the sample names and differs from the JSON files of GN and R/qtl2.
+#
+# Pjotr Prins (c) 2024
+
+import sys
+import argparse
+import json
+# import lmdb
+# import pandas as pd
+from struct import *
+
+import pandas as pd
+
+meta = { "type": "gn-geno-to-gemma"
+         }
+
+parser = argparse.ArgumentParser(description='Turn GN geno format into GEMMA BIMBAM format + JSON')
+parser.add_argument('file',help="GN geno file")
+args = parser.parse_args()
+
+genofn = args.file
+
+meta["genofile"] = genofn
+translate = { "A": "0", "D": "0", "B": "1", "H": "0.5", "U": "NA" }
+header = []
+
+with open(genofn) as f:
+    for line in f:
+        first = line[0]
+        if first in ["#","@"]:
+            header.append(line.rstrip())
+            continue
+        fields = line.rstrip().split("\t")
+        if fields[0] == "Chr":
+            # print(fields)
+            samples = fields[4:-1]
+            meta["samples"] = samples
+            meta["numsamples"] = len(samples)
+            continue
+        # continue processing genotypes
+        chrom,marker,cm,mb,*gns = fields
+        # print(gns)
+        outfields = [marker,"X","Y"] + [translate[item] for item in gns]
+        print(",".join(outfields))
+
+meta["header"] = header
+print(json.dumps(meta),file=sys.stderr)
+
+with open(genofn+".json","w") as f:
+    f.write(json.dumps(meta))
--- bin/rqtl2-pheno-to-gemma.py
+++ bin/rqtl2-pheno-to-gemma.py
@@ -3,8 +3,6 @@
 # Takes an R/qtl2 type CSV and writes the GEMMA file checking for the genotype/genometype names
 # and writing an ordered list to stdout. Usage:
 #
-# rqtl2-pheno-to-gemma.py infile.csv
-#
 # Note that GEMMA can take multi-column phenotype files and picks with the -n switch
 # See also https://issues.genenetwork.org/topics/data/R-qtl2-format-notes
 #
@@ -27,9 +25,25 @@ csv = pd.read_csv(args.file,index_col=0,na_values=['x','NA','-',''])
 js = json.load(open("BXD.json"))
 samples = js["genofile"][0]["sample_list"] # list individuals
 
+sampleset = set(samples)
+csvset = set(csv.index)
+
+if sampleset.difference(csvset):
+    print(f"ERROR: sets differ {sampleset.difference(csvset)}",file=sys.stderr)
+    sys.exit(2)
+
+if csvset.difference(sampleset):
+    print(f"WARNING: sets differ, we'll ignore inputs from {args.file} {csvset.difference(sampleset)}",file=sys.stderr)
+
+# if len(samples) != len(csv.index): # superfluous with above
+#     print(f"ERROR: sizes do not match len(samples)={len(samples)} and len(csv.index)={len(csv.index)}",file=sys.stderr)
+#     sys.exit(2)
+
 l = [] # generate genotype ordered sample output
 for s in samples:
     l.append(csv.loc[s])
 
 out = pd.DataFrame(l)
 out.to_csv(sys.stdout,na_rep='NA',sep="\t",index=False,header=False)
+
+print(f"Wrote GEMMA pheno {len(l)} rows and {len(out.columns)} cols!",file=sys.stderr)
