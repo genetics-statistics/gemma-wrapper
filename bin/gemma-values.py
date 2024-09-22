a4db53bef06b526aae098a05b6b8859d54253ca3
Pjotr Prins <pjotr.public01@thebird.nl>
2024-09-22 08:11:43 +0200

    Use tmpdir to unpack tar ball

--- bin/gemma-values.py
+++ bin/gemma-values.py
@@ -18,9 +18,10 @@ import json
 import lmdb
 import math
 import os
-import re
 from pathlib import Path
+import re
 from struct import *
+import tempfile
 
 from signal import signal, SIGPIPE, SIG_DFL # avoid broken pipe error
 signal(SIGPIPE,SIG_DFL)
@@ -44,41 +45,43 @@ if args.anno:
 
 print("chr,pos,marker,af,beta,se,l_mle,l_lrt,-logP")
 
-fn = args.lmdb
-if fn.endswith('.xz'):
-    print(f"Unpack {fn}...",file=sys.stderr)
-    os.system(f"tar xvJf {fn} > /dev/null")
-    fn = Path(Path(fn).stem).stem+".mdb"
+with tempfile.TemporaryDirectory() as tmpdir:
+    print(f"Created temporary directory {tmpdir}",file=sys.stderr)
 
-print(f"Reading {fn}...",file=sys.stderr)
-# sys.exit(1)
+    fn = args.lmdb
+    if fn.endswith('.xz'):
+        print(f"Unpack {fn}...",file=sys.stderr)
+        os.system(f"tar xvJf {fn} -C {tmpdir} > /dev/null")
+        fn = tmpdir+"/"+Path(Path(fn).stem).stem+".mdb"
 
-result = []
-with lmdb.open(fn,subdir=False) as env:
-    with env.begin() as txn:
-        with txn.cursor() as curs:
-            # quick check and output of keys
-            for key in list(txn.cursor().iternext(values=False)):
-                if key != b'meta':
-                    chr1,pos = unpack('>cL',key)
-                    chr2 = int.from_bytes(chr1,"little")
-                    # chr2 = chr1
-                    if chr2 == X:
-                        chr = "X"
-                    elif chr1 == Y:
-                        chr = "Y"
-                    else:
-                        chr = str(chr2)
-                    rec = txn.get(key)
-                    af,beta,se,l_mle,p_lrt = unpack('=fffff',rec)
-                    effect = -beta/2.0
-                    minusLogP = -math.log(p_lrt,10)
-                    snp = "?"
-                    if args.anno:
-                        key2 = chr+":"+str(pos)
-                        snp = snps[key2]
-                    result.append([chr,str(pos),snp,str(round(af,4)),str(round(effect,4)),
-                                    str(round(se,4)),str(round(l_mle,4)),str(round(p_lrt,4)),str(round(minusLogP,2))])
+    print(f"Reading {fn}...",file=sys.stderr)
+
+    result = []
+    with lmdb.open(fn,subdir=False) as env:
+        with env.begin() as txn:
+            with txn.cursor() as curs:
+                # quick check and output of keys
+                for key in list(txn.cursor().iternext(values=False)):
+                    if key != b'meta':
+                        chr1,pos = unpack('>cL',key)
+                        chr2 = int.from_bytes(chr1,"little")
+                        # chr2 = chr1
+                        if chr2 == X:
+                            chr = "X"
+                        elif chr1 == Y:
+                            chr = "Y"
+                        else:
+                            chr = str(chr2)
+                        rec = txn.get(key)
+                        af,beta,se,l_mle,p_lrt = unpack('=fffff',rec)
+                        effect = -beta/2.0
+                        minusLogP = -math.log(p_lrt,10)
+                        snp = "?"
+                        if args.anno:
+                            key2 = chr+":"+str(pos)
+                            snp = snps[key2]
+                        result.append([chr,str(pos),snp,str(round(af,4)),str(round(effect,4)),
+                                       str(round(se,4)),str(round(l_mle,4)),str(round(p_lrt,4)),str(round(minusLogP,2))])
 
 if args.sort:
     result = sorted(result, key=lambda x: x[8], reverse=True)
