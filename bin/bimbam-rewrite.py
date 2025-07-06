238407b79ab94f38c519f6eb9f9cf85c68a94483
Pjotr Prins <pjotr.public01@thebird.nl>
2025-07-06 10:09:06 +0200

    bimbam rewrites

--- bin/bimbam-rewrite.py
+++ bin/bimbam-rewrite.py
@@ -14,20 +14,39 @@ import re
 # import pandas as pd
 
 parser = argparse.ArgumentParser(description='Rewrite BIMBAM')
+parser.add_argument('--inject',help="Inject genotypes from file")
+parser.add_argument('--randomize', action=argparse.BooleanOptionalAction,help="Randomize data (default is inject haplotypes)")
+parser.add_argument('--perc',type=float, default=None,help="Percentage to replace")
 parser.add_argument('file',help="BIMBAM file")
 args = parser.parse_args()
 
+injectfn = args.inject
+randomize = args.randomize
 genofn = args.file
+perc = args.perc
+inject = []
 
+if injectfn:
+    with open(injectfn) as f:
+        for line in f:
+            fields = re.split(r"[, \t]+", line.rstrip())
+            marker,x1,x2,*gns = fields
+            # inject = [int(item) for item in gns]
+            inject = gns
+            # print(gns)
+
+count = 0
 with open(genofn) as f:
     for line in f:
+        count += 1
         fields = re.split(r"[, \t]+", line.rstrip())
-        # print(fields)
-        # assert len(fields) == count_all_samples+3, f"{len(fields)} != {count_all_samples+3}"
         marker,x1,x2,*gns = fields
-        # print(gns)
-        gns = [str(round(random.uniform(0,2),2)) for item in gns]
-        # print(gns)
+        if (perc is None) or random.uniform(0,100) < perc:
+            if randomize:
+                gns = [str(round(random.uniform(0,2),2)) for item in gns]
+            else:
+                gns = inject
         outfields = [marker,x1,x2] + gns
         print(",".join(outfields))
-        # sys.exit(1)
+        # if count>30:
+        #     sys.exit(1)
