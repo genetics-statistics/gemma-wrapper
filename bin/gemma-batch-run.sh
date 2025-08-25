9a4672f1d32d54d467b3af0b7bba3b41088fa5b1
Pjotr Prins <pjotr.public01@thebird.nl>
2025-08-25 02:21:52 -0500

    Moved trait generation into gemma-wrapper

--- bin/gemma-batch-run.sh
+++ bin/gemma-batch-run.sh
@@ -9,7 +9,6 @@ for id in `cat ids.txt` ; do
   echo Precomputing $id 
   if [ ! -e tmp/*-BXDPublish-$id-gemma-GWA.tar.xz ] ; then
     curl http://127.0.0.1:8092/dataset/bxd-publish/values/$id.json > pheno.json
-    ./bin/gn-pheno-to-gemma.rb --phenotypes pheno.json --geno-json BXD.geno.json > BXD_pheno.txt
-    ./bin/gemma-wrapper --json --lmdb --population BXD --name BXDPublish --trait $id --loco --input K.json -- -g BXD.geno.txt -p BXD_pheno.txt -a BXD.8_snps.txt -lmm 9 -maf 0.1 -n 2 -debug > GWA.json
+    ./bin/gemma-wrapper --json --lmdb --geno-json BXD.geno.json --phenotypes pheno.json --population BXD --name BXDPublish --trait $id --loco --input K.json -- -g BXD.geno.txt -a BXD.8_snps.txt -lmm 9 -maf 0.1 -n 2 -debug > GWA.json
   fi
 done
