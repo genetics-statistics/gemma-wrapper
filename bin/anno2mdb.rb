424efb8ba9457ceef2992a16e5e72d605b47fc1e
Pjotr Prins <pjotr.public01@thebird.nl>
2025-11-12 12:10:12 +0100

    Storing the SNP/marker data as LMDB speeds things up!

---
+++ bin/anno2mdb.rb
@@ -0,0 +1,97 @@
+#!/usr/bin/env ruby
+#
+# Convert a (snp) annotation file to lmdb - ends up being larger, but maybe faster
+#
+# If you get a compatibility error in guix you may need an older Ruby. Otherwise you can do:
+#
+#   env GEM_PATH=tmp/ruby GEM_HOME=tmp/ruby gem install lmdb
+#   env GEM_PATH=tmp/ruby ruby -e "require 'lmdb'"
+#
+# Pjotr Prins (c) 2025
+
+require 'tmpdir'
+require 'json'
+require 'lmdb'
+require 'optparse'
+require 'socket'
+
+options = { show_help: false }
+
+opts = OptionParser.new do |o|
+  o.banner = "\nUsage: #{File.basename($0)} [options] filename(s)"
+
+  # o.on('-a','--anno FILEN', 'Annotation file') do |anno|
+  #   options[:anno] = anno
+  #   raise "Annotation input file #{anno} does not exist" if !File.exist?(anno)
+  # end
+
+  # o.on("-v", "--verbose", "Run verbosely") do |v|
+  #   options[:verbose] = true
+  # end
+
+  # o.on("-d", "--debug", "Show debug messages and keep intermediate output") do |v|
+  #   options[:debug] = true
+  # end
+
+  o.separator ""
+
+  o.on_tail('-h', '--help', 'display this help and exit') do
+    options[:show_help] = true
+  end
+end
+
+opts.parse!(ARGV)
+
+if options[:show_help]
+  print opts
+  # print USAGE
+  exit 1
+end
+
+X='X'.ord
+Y='Y'.ord
+M='M'.ord
+
+ARGV.each do |fn|
+  $stderr.print "Reading #{fn}\n"
+  mdb = fn + ".mdb"
+  $stderr.print("lmdb #{mdb}...\n")
+  env = LMDB.new(mdb, nosubdir: true, mapsize: 10**9)
+  maindb = env.database
+  db = env.database(File.basename(mdb), create: true)
+
+  File.open(fn).each_line do |line|
+    snp,pos,chr = line.split(/[\s,]+/)
+    location = "#{chr}:#{pos}"
+    chr_c =
+      if chr == "X"
+        X
+      elsif chr == "Y"
+        Y
+      elsif chr == "M"
+        M
+      else
+        chr.to_i
+      end
+    key = [chr_c,pos.to_i].pack("cL>")
+    # env.transaction do |child1|
+    db[key] = snp
+    db.put "HEY","HEY"
+    # end
+  end
+  p db.size
+  env.close
+
+  fn = fn+".mdb"
+  $stderr.print "Reading #{fn}\n"
+  snp_env = LMDB.new(fn, nosubdir: true)
+  snp_db = snp_env.database(File.basename(fn), :create=>false)
+  chr_c = "1".to_i
+  pos = 3231204
+  key = [chr_c,pos.to_i].pack("cL>")
+  marker_name = snp_db[key]
+  p marker_name
+  key = "HEY"
+  marker_name = snp_db.get key
+  p marker_name
+end
